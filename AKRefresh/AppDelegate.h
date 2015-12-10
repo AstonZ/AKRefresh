87ce7bcbd96a4968f5717fc5c496ea40165b72f2
Aston Zhang <liangziokay@gmail.com>
2015-12-10 16:14:53 +0800

    Initial Commit

---
+++ AKRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKRefresh
+//
+//  Created by 张良 on 15/12/10.
+//  Copyright © 2015年 Aston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AKRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKRefresh
+//
+//  Created by 张良 on 15/12/10.
+//  Copyright © 2015年 Aston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
